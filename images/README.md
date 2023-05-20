9e0009236bc30f2f6f0ce438daa8b7332d4c822e
Ranjeet Singh <ranjeetsinghfattu@gmail.com>
2023-05-20 22:47:36 +0530

    Change the Entire html,css,js

---
+++ images/README.md
@@ -0,0 +1,2 @@
+# Learning
+- Math.floor(Math.random() * 6)+1; // Returns a random integer from 1 to 6:
--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-"use strict";
-const images=["./images/dice1.png","./images/dice2.png","./images/dice3.png","./images/dice4.png","./images/dice5.png","./images/dice6.png"]
-let value1= Math.floor(Math.random()*6+1) ;
-let value2= Math.floor(Math.random()*6+1) ;
-if (value1==value2) {
-    document.getElementById("h1").innerHTML="Draw!";
-    document.getElementById("pic1").src=images[value1-1];
-    document.getElementById("pic2").src=images[value2-1]
-} 
-else if(value1>value2) {
-    document.getElementById("h1").innerHTML="🚩 Play 1 Wins!";
-    document.getElementById("pic1").src=images[value1-1];
-    document.getElementById("pic2").src=images[value2-1]
-}
-else{
-    document.getElementById("h1").innerHTML="Player 2 Wins! 🚩";
-    document.getElementById("pic1").src=images[value1-1];
-    document.getElementById("pic2").src=images[value2-1]
+const handleClick=()=>{
+Dice1 = Math.floor(Math.random() * 6) + 1 // to generate a random number from 1-6
+Dice2 = Math.floor(Math.random() * 6) + 1
+
+if (Dice1 > Dice2)
+    document.querySelector('.output').innerHTML = '🚩 Player-1 Wins'
+else if (Dice1 == Dice2)
+    document.querySelector('.output').innerHTML = 'Draw'
+else
+    document.querySelector('.output').innerHTML = '🚩 Player-2 Wins'
+
+document.querySelector('.image-1').attributes.src.value=`./images/dice${Dice1}.png`;
+document.querySelector('.image-2').attributes.src.value=`./images/dice${Dice2}.png`;
 }
